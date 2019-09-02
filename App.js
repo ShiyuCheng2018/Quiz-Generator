b0fa6a723a1b404bb315f1154f34ef0d2f76d076
Shiyu Cheng <starkeyshiyu@gmail.com>
2019-09-02 12:14:11 -0700

    transfer navbar and head to be fixed on the top

--- App.js
+++ App.js
@@ -18,24 +18,25 @@ function fetchQuestions(){
 }
 
 function getQuestions(response) {
+
     function injectQuestions(question, answer, index){
         let injection = "<!--\t\t\t\t\t\t\t\tsingle-question-starts-->\n" +
             "<div class=\"form-group question shadow p-3 mb-5 bg-white rounded\">\n" +
             "\t\t\t\t\t\t\t\t\t<h4>"+index+". "+question+"</h4>\n" +
             "\t\t\t\t\t\t\t\t\t<hr />\n" +
             "\t\t\t\t\t\t\t\t\t<div class=\"form-check form-check-inline\">\n" +
-            "\t\t\t\t\t\t\t\t\t\t<input class=\"form-check-input\" type=\"radio\" name=\"opt\" value=\"true\">\n" +
+            "\t\t\t\t\t\t\t\t\t\t<input class=\"form-check-input\" type=\"radio\" name=\"opt"+index+"\" value=\"true\">\n" +
             "\t\t\t\t\t\t\t\t\t\t<label class=\"form-check-label\" >True</label>\n" +
             "\t\t\t\t\t\t\t\t\t</div>\n" +
             "\t\t\t\t\t\t\t\t\t<div class=\"form-check form-check-inline\">\n" +
-            "\t\t\t\t\t\t\t\t\t\t<input class=\"form-check-input\" type=\"radio\" name=\"opt\" value=\"false\">\n" +
+            "\t\t\t\t\t\t\t\t\t\t<input class=\"form-check-input\" type=\"radio\" name=\"opt"+index+"\" value=\"false\">\n" +
             "\t\t\t\t\t\t\t\t\t\t<label class=\"form-check-label\" >False</label>\n" +
             "\t\t\t\t\t\t\t\t\t</div>\n" +
             "\t\t\t\t\t\t\t\t</div>"+
             "<!--\t\t\t\t\t\t\t\tsingle-question-ends-->\n";
         $("#question-form").append(injection);
     }
-
+    // The index of question
     let index = 1;
     response.results.map(question => {
         injectQuestions(question.question,question.correct_answer, index);
